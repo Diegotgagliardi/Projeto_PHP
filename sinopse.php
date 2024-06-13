3606f40272bc18a4dd3de454d079fc421103bbd6
Alantrizotte06 <trizotte.alan@gmail.com>
2024-06-13 03:47:05 -0300

    Alterções

--- sinopse.php
+++ sinopse.php
@@ -13,13 +13,29 @@
     <div>
       <img class="fundo_filme" src="img/oppenheimer.jpg">
     </div>
+    <div class="tit_filme">
+      <h1>Oppenheimer</h1>
+    </div>
+    <div class="btns">
+      <button type="button" class="btn_watch">Assistir Trailer</button>
+    </div>
     <div class="text_sinopse">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quos aspernatur eligendi sapiente fuga vel
         corporis sunt sed amet blanditiis temporibus illum, nulla perferendis cupiditate delectus voluptatibus nemo.
         Laboriosam sunt consequuntur totam esse mollitia soluta, culpa debitis nihil sint natus accusamus ex nulla
         deserunt id magni excepturi non tenetur aperiam!</p>
     </div>
+    <div class="info_filme">
+      <p>Diretor: Christopher Nolan</p>
+      <p>Elenco: Cillian Murphy, Florence Pugh, Robert Downey Jr., Emily Blunt, Josh Hartnett, Matt Damon e mais</p>
+      <p>Gênero: Suspense/Obra de Época</p>
+    </div>
   </div>
+  <footer>
+    <div class="tit_rodape">
+      <h1>Algoflix©</h1>
+    </div>
+  </footer>
 </body>
 
 </html>
