97d5f780fe77b6d5b9812bcb48998bbc99d29d8e
Alantrizotte06 <trizotte.alan@gmail.com>
2024-06-13 02:22:41 -0300

    Teste

---
+++ sinopse.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS/sinopse.css">
+  <title>Algoflix</title>
+</head>
+
+<body>
+  <div class="fundo">
+    <div>
+      <img class="fundo_filme" src="img/oppenheimer.jpg">
+    </div>
+    <div class="text_sinopse">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quos aspernatur eligendi sapiente fuga vel
+        corporis sunt sed amet blanditiis temporibus illum, nulla perferendis cupiditate delectus voluptatibus nemo.
+        Laboriosam sunt consequuntur totam esse mollitia soluta, culpa debitis nihil sint natus accusamus ex nulla
+        deserunt id magni excepturi non tenetur aperiam!</p>
+    </div>
+  </div>
+</body>
+
+</html>
